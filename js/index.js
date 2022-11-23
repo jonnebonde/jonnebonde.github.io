d548edc1d9ff487109cd4cfdf068fb4d5e748a1a
jonnebonde <jonnebonde@gmail.com>
2022-11-23 21:15:40 +0100

    added responsive nav and changes on scroll

---
+++ js/index.js
@@ -0,0 +1,38 @@
+const toggleButton = document.querySelector(".nav-hamburgermenu-icon");
+const navMenu = document.querySelector(".container");
+const navContainer = document.querySelector("nav");
+
+toggleButton.addEventListener("click", () => {
+  toggleButton.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.addEventListener("click", function (e) {
+  if (!navContainer.contains(e.target)) {
+    navMenu.classList.remove("active");
+    toggleButton.classList.remove("active");
+  }
+});
+
+
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav .container ul li");
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight / 4) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("activeLink");
+    if (li.classList.contains(current)) {
+  
+      li.classList.add("activeLink");
+    }
+  });
+}
