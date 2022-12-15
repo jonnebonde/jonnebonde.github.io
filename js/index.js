321db30f6224d019b48296fe44822b140a527375
jonnebonde <jonnebonde@gmail.com>
2022-12-15 23:07:18 +0100

    added slider, fixed content and responsiveness.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const toggleButton = document.querySelector(".nav-hamburgermenu-icon");
 const navMenu = document.querySelector(".nav-link-container");
 const navContainer = document.querySelector("nav");
+const linkButton = document.querySelectorAll(".nav-link-container a");
 
 toggleButton.addEventListener("click", () => {
   toggleButton.classList.toggle("active");
@@ -14,6 +15,13 @@ document.addEventListener("click", function (e) {
   }
 });
 
+linkButton.forEach(function (x) {
+  x.onclick = function() {
+    toggleButton.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  }
+})
+  
 
 const sections = document.querySelectorAll("section");
 const navLi = document.querySelectorAll("nav .nav-link-container ul li");
@@ -35,3 +43,50 @@ window.onscroll = () => {
     }
   });
 }
+
+
+const items = document.querySelectorAll(".card");
+const itemCount = items.length;
+const nextItem = document.querySelector(".next");
+const previousItem = document.querySelector(".previous");
+let count = 0;
+
+function showNextItem() {
+  items[count].classList.remove('active');
+
+  if(count < itemCount - 1) {
+    count++;
+  } else {
+    count = 0;
+  }
+
+  items[count].classList.add('active');
+  console.log(count);
+}
+
+function showPreviousItem() {
+  items[count].classList.remove('active');
+
+  if(count > 0) {
+    count--;
+  } else {
+    count = itemCount - 1;
+  }
+
+  items[count].classList.add('active');
+  console.log(count);
+}
+
+function keyPress(e) {
+  e = e || window.event;
+  
+  if (e.keyCode == '37') {
+    showPreviousItem();
+  } else if (e.keyCode == '39') {
+    showNextItem();
+  }
+}
+
+nextItem.addEventListener('click', showNextItem);
+previousItem.addEventListener('click', showPreviousItem);
+document.addEventListener('keydown', keyPress);
