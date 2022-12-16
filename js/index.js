ba216a6e3b7f5e96ddc2ad52212ad7f5976db487
jonnebonde <jonnebonde@gmail.com>
2022-12-16 22:52:07 +0100

    Fixed responsiveness and added content

--- js/index.js
+++ js/index.js
@@ -22,7 +22,6 @@ linkButton.forEach(function (x) {
   }
 })
 
-
 const sections = document.querySelectorAll("section");
 const navLi = document.querySelectorAll("nav .nav-link-container ul li");
 window.onscroll = () => {
@@ -43,52 +42,6 @@ window.onscroll = () => {
     }
   });
 }
-
-const items = document.querySelectorAll(".card");
-const itemCount = items.length;
-const nextItem = document.querySelector(".next");
-const previousItem = document.querySelector(".previous");
-let count = 0;
-
-function showNextItem() {
-  items[count].classList.remove('active');
-
-  if (count < itemCount - 1) {
-    count++;
-  } else {
-    count = 0;
-  }
-
-  items[count].classList.add('active');
-  console.log(count);
-}
-
-function showPreviousItem() {
-  items[count].classList.remove('active');
-
-  if (count > 0) {
-    count--;
-  } else {
-    count = itemCount - 1;
-  }
-
-  items[count].classList.add('active');
-  console.log(count);
-}
-
-function keyPress(e) {
-  e = e || window.event;
-
-  if (e.keyCode == '37') {
-    showPreviousItem();
-  } else if (e.keyCode == '39') {
-    showNextItem();
-  }
-}
-
-nextItem.addEventListener('click', showNextItem);
-previousItem.addEventListener('click', showPreviousItem);
-document.addEventListener('keydown', keyPress);
 
 
 const contactFormUrl = "https://jonnekrosby.site/project-exam1/wp-json/contact-form-7/v1/contact-forms/190/feedback";
@@ -119,10 +72,10 @@ let successMessage = false;
 function validateInputs(event) {
   event.preventDefault();
 
-  const name = checkLength(nameInput.value, 4);
+  const name = checkLength(nameInput.value, 1);
   const email = validateEmail(emailInput.value);
-  const subject = checkLength(subjectInput.value, 14);
-  const message = checkLength(messageInput.value, 24);
+  const subject = checkLength(subjectInput.value, 7);
+  const message = checkLength(messageInput.value, 9);
   const hidden = checkLength(hiddenInput.value, 1);
 
   if (name) {
